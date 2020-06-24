cd4ed83c1027137f55f3dadf0a2fadca92df8991
Benjamin Cisneros <bcisneros@tiempodevelopment.com>
2020-06-24 04:56:42 -0600

    Setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,23 @@
 module.exports = {
-  extends: ["eslint:recommended"],
+  plugins: ["react"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
-    ecmaVersion: 6
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   env: {
     node: true
+  },
+  rules: {
+    quotes: ["error", "single", { avoidEscape: true }],
+    "comma-dangle": ["error", "always-multiline"]
+  },
+  settings: {
+    react: {
+      version: "16.13.1"
+    }
   }
 };
