f10aa60ea8d9bb022b9b2b8875169f0b624413de
Benjamin Cisneros <bcisneros@tiempodevelopment.com>
2020-06-24 06:30:20 -0600

    Add new symbol when search

--- src/App.js
+++ src/App.js
@@ -4,30 +4,36 @@ import StockSearch from './components/StockSearch';
 import StockList from './components/StockList';
 import StockGraph from './components/StockGraph';
 
+const initialStocks = [
+  {
+    description: 'AGILENT TECHNOLOGIES INC',
+    displaySymbol: 'A',
+    symbol: 'A',
+  },
+  {
+    description: 'ALCOA CORP',
+    displaySymbol: 'AA',
+    symbol: 'AA',
+  },
+  {
+    description: 'PERTH MINT PHYSICAL GOLD ETF',
+    displaySymbol: 'AAAU',
+    symbol: 'AAAU',
+  },
+];
+
 function App() {
   const handleSearch = symbol => {
     console.log(symbol);
+    setStocks([
+      ...stocks,
+      { symbol, description: symbol, displaySymbol: symbol },
+    ]);
   };
   const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
-    setStocks([
-      {
-        description: 'AGILENT TECHNOLOGIES INC',
-        displaySymbol: 'A',
-        symbol: 'A',
-      },
-      {
-        description: 'ALCOA CORP',
-        displaySymbol: 'AA',
-        symbol: 'AA',
-      },
-      {
-        description: 'PERTH MINT PHYSICAL GOLD ETF',
-        displaySymbol: 'AAAU',
-        symbol: 'AAAU',
-      },
-    ]);
+    setStocks(initialStocks);
   }, []);
 
   return (
