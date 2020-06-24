013a7ed14dc631eed88dad2a697980881e22ccdd
Benjamin Cisneros <bcisneros@tiempodevelopment.com>
2020-06-24 05:47:40 -0600

    Create main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React from 'react';
+
+import StockSearch from './components/StockSearch';
+import StockList from './components/StockList';
+import StockGraph from './components/StockGraph';
 
 function App() {
   return (
     <div className="App">
       <h1>Dolphin</h1>
+      <StockSearch />
+      <StockList />
+      <StockGraph />
     </div>
   );
 }
---
+++ src/components/StockGraph.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StockGraph = () => {
+  return <div>Stock Graph</div>;
+};
+
+export default StockGraph;
